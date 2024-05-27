2719e55c0f2592458fb1ec8b5fcd50d7409167d9
gheeeeeeee <gheoputra55@gmail.com>
2024-05-27 14:56:45 +0700

    sama dengan project overiding bedanya letak typedata final berada disamping baseCLass

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan()  {
+        cout << "halo saya function dari base class";
+    }
+};
+
+class derivedclass : public baseClass {
+public:
+    void perkenalan() {
+        cout << " hello sayaa function dari derived Class";
+    }
+};
+
+
+int main()
+{
+    derivedclass a;
+    a.perkenalan();
+
+    return 0;
+}
